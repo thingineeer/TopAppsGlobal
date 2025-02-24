4f0bd953bdf2b1330ed401cbc54a57a03e196ad4
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-24 23:30:24 +0900

    [Feat] #3 - implement AppStoreRepository

--- TopAppsGlobal-iOS/TopAppsGlobal-iOS/Data/Repositories/AppStoreRepositoryImpl.swift
+++ TopAppsGlobal-iOS/TopAppsGlobal-iOS/Data/Repositories/AppStoreRepositoryImpl.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class AppStoreRepositoryImpl: AppStoreRepository {
+    func fetchApps(country: Country) async throws -> [AppEntity] {
+        guard let url = APIEndpoints.topFreeApps(country: country) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200..<300).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let dto = try JSONDecoder().decode(AppStoreResponseDTO.self, from: data)
+        return dto.toDomain()
+    }
+}
