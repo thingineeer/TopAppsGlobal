7af1c6a872e569a4eab6cb1a93c821cadb46e4f6
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-24 23:29:16 +0900

    [Add] #3 - add AppStoreEntity

---
+++ TopAppsGlobal-iOS/TopAppsGlobal-iOS/Domain/Entities/AppEntity.swift
@@ -0,0 +1,19 @@
+//
+//  AppEntity.swift
+//  TopAppsGlobal-iOS
+//
+//  Created by 이명진 on 2/24/25.
+//
+
+import Foundation
+
+struct AppEntity: Identifiable {
+    let id: String
+    let name: String
+    let developer: String
+    let category: String
+    let imageUrl: String
+    let summary: String
+    let releaseDate: String
+    let appStoreUrl: String
+}
