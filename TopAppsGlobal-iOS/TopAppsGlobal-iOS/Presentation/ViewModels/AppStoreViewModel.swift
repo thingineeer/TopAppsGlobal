5a00f7498af902f0af03d340b510cdd0fa998bf2
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-24 23:31:33 +0900

    [Feat] #3 - implement AppStore ViewModel

--- TopAppsGlobal-iOS/TopAppsGlobal-iOS/Presentation/ViewModels/AppStoreViewModel.swift
+++ TopAppsGlobal-iOS/TopAppsGlobal-iOS/Presentation/ViewModels/AppStoreViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+@MainActor
+final class AppStoreViewModel: ObservableObject {
+    @Published var apps: [AppEntity] = []
+    @Published var isLoading = false
+    @Published var showError = false
+    @Published var errorMessage = ""
+    
+    private let fetchAppsUseCase: FetchAppsUseCase
+    
+    init(fetchAppsUseCase: FetchAppsUseCase) {
+        self.fetchAppsUseCase = fetchAppsUseCase
+    }
+    
+    func fetchApps(for countryIndex: Int) async {
+        guard let country = Country(rawValue: countryIndex) else { return }
+        
+        isLoading = true
+        do {
+            apps = try await fetchAppsUseCase.execute(country: country)
+            isLoading = false
+        } catch {
+            isLoading = false
+            showError = true
+            errorMessage = error.localizedDescription
+        }
+    }
+}
