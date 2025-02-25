35f652da6047c40f73056fada528173e7306a3aa
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-25 19:38:36 +0900

    [Feat] #3 - implement myPageView

--- TopAppsGlobal-iOS/TopAppsGlobal-iOS/Presentation/Views/MyPageView.swift
+++ TopAppsGlobal-iOS/TopAppsGlobal-iOS/Presentation/Views/MyPageView.swift
@@ -12,8 +12,29 @@ struct MyPageView: View {
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0"
     
     var body: some View {
-        HStack {
-            
+        NavigationStack {
+            List {
+                
+                Section {
+                    Link(destination: URL(string: Config.privateInfoURL)!) {
+                        HStack {
+                            Text("개인정보 처리방침")
+                            Spacer()
+
+                        }
+                    }
+                }
+                
+                Section {
+                    HStack {
+                        Text("버전")
+                        Spacer()
+                        Text(appVersion)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .navigationTitle("마이페이지")
         }
     }
 }
