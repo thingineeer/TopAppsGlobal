651bc4d0ce21a8f31935da0ae40b129a17d085b5
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-25 17:52:16 +0900

    [Feat] #3 - add paidApp category

--- TopAppsGlobal-iOS/TopAppsGlobal-iOS/Common/AppType.swift
+++ TopAppsGlobal-iOS/TopAppsGlobal-iOS/Common/AppType.swift
@@ -10,11 +10,13 @@ import Foundation
 enum AppType {
     case free
     case paid
+    case my
     
     var title: String {
         switch self {
         case .free: return "무료 앱 순위"
         case .paid: return "유료 앱 순위"
+        case .my: return "마이페이지"
         }
     }
 }
